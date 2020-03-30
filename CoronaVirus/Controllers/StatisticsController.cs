b8d78f83d158226a01529e97a64b19856bf1a5a1
Sam <sam.j1912@hotmail.com>
2020-03-29 22:40:13 -0400

    First push

---
+++ CoronaVirus/Controllers/StatisticsController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace CoronaVirus.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticsController : ControllerBase
+    {
+       
+        private IMemoryCache _memoryCache;
+
+        public StatisticsController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet]
+        public Statistic Get ()
+        {
+
+            return GetStatistic();
+        }
+
+        private Statistic GetStatistic()
+        {
+            Statistic statistic = _memoryCache.Get<Statistic>("statistic"); 
+            if (statistic == null || statistic.LastUpdatedUTC < DateTime.UtcNow.AddHours(-2) )
+            {
+                string confirmed = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+                string deaths = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv";
+                string healed = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv";
+
+                statistic = new Statistic();
+                statistic.Confirmed = GetDayItems(confirmed);
+                statistic.Deaths = GetDayItems(deaths);
+                statistic.Healed = new[] { new DayItem { Count = 0, Date = "" } };
+                _memoryCache.Set<Statistic>("statistic", statistic);
+                return statistic;
+            }
+
+            return statistic;
+        }
+
+        private IEnumerable<DayItem> GetDayItems (string fileUrl )
+        {
+            using (WebClient webclient = new WebClient())
+            {
+                var fileText = webclient.DownloadString(fileUrl);
+                using (CsvHelper.CsvReader csvReader = new CsvReader(new StreamReader(GenerateStreamFromString(fileText)), new CsvConfiguration(System.Globalization.CultureInfo.CurrentCulture)))
+                {
+
+                    while (csvReader.Read())
+                    {
+                        var record = csvReader.GetRecord<dynamic>();
+                        if (csvReader.GetField(1) == "Iraq")
+                        {
+                            foreach (var property in (record as IDictionary<string, object>).Skip(36))
+                            {
+                        yield     return    (new DayItem { Date = property.Key, Count = int.Parse(property.Value.ToString()) });
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        public static Stream GenerateStreamFromString(string s)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+    }
+   
+
+
+    public class Statistic
+    {
+        public Statistic()
+        {
+            Confirmed = new List<DayItem>();
+            Deaths = new List<DayItem>();
+            Healed = new List<DayItem>();
+            LastUpdatedUTC = DateTime.UtcNow;
+        }
+        public IEnumerable<DayItem> Confirmed { get; set; }
+        public IEnumerable<DayItem> Deaths { get; set; }
+        public IEnumerable<DayItem> Healed { get; set; }
+        public DateTime LastUpdatedUTC { get; }
+    }
+
+    public class DayItem
+    {
+        public string Date { get; set; }
+        public int Count  { get; set; }
+
+    }
+}
---
+++ CoronaVirus/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace CoronaVirus
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<MemoryCache>();
+            services.AddMvc((MvcOptions o) => { o.EnableEndpointRouting = false;
+                
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseCors(( CorsPolicyBuilder c) =>{
+                c.SetIsOriginAllowed((string origin) => {
+                    if (origin.Contains("coronavirusiraq.me"))
+                        return true;
+                    return  true;
+                });
+                c.AllowAnyHeader();
+                c.AllowAnyMethod(); 
+
+            });
+            app.UseMvc();
+
+        }
+    }
+}
